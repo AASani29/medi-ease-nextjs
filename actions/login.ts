73baaa1fbd252407216520847e25d4b8692d093e
Hasin <hasinmahtab.alvee@gmail.com>
2024-06-02 17:16:07 +0600

    fixed login

--- actions/login.ts
+++ actions/login.ts
@@ -1,9 +1,13 @@
 "use server";
 
 import { signIn } from "@/auth";
+import { getUserByEmail } from "@/data/user";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
 import type * as z from "zod";
+
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
 	const validatedFields = LoginSchema.safeParse(values);
@@ -13,6 +17,11 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 	}
 
 	const { email, password } = validatedFields.data;
+
+	const existingUser = await getUserByEmail(email);
+	if (!existingUser || !bcrypt.compareSync(password, existingUser.password)) {
+		return { Error: "Invalid credentials" };
+	}
 
 	const user = await signIn("credentials", {
 		email,
